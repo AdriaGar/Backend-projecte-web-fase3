904f0324d6277e3c3fd1083af4a33bb361e0cd50
Òscar Mollinedo Echevarría <oscar.mollinedo@institutvidreres.cat>
2025-02-08 14:11:23 +0100

    Crear dos enpoints, el primer "/mail" per enviar un correu al usuari demanat, i el segon '/usuaris/recuperacio' per retornar la clau unica d'un usuari

--- server.js
+++ server.js
@@ -17,9 +17,11 @@ const port = 3080;
 
 var transporter = nodemailer.createTransport({
     service: 'localhost',
+    port: 25,
+    secure: false,
     auth: {
-        user: '',
-        pass: ''
+        user: 'admin',
+        pass: 'admin'
     }
 });
 const db = admin.firestore();
@@ -34,23 +36,79 @@ app.get('/exemple', async (req, res) => {
     res.json(datos);
 });
 
-app.post('/correu', async (req, res) => {
-    let correu = req.body;
+app.post('/mail', async (req, res) => {
+    let usuari = req.body;
+
+    const document = await dbC.doc(usuari.usuariid.trim()).get();
+    
+    const dades = document.data();
+    
+    console.log(dades)
+    
     let mailOptions = {
-        from: '',
-        to: 'req.body',
-        subject: 'Correu recuperacio compte',
-        text: 'aixo es un correu per actualitzar la teva compte'
+        from: 'BobbyCotxes@cotxesbobby.com',
+        to: dades.correo,
+        subject: 'Correu recuperacio compte de ECommerce Bobby Cotxes',
+        html: `<!DOCTYPE html>
+        <html lang="ca">
+            <head>
+                <meta charset="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Bobby Cotxes: Recuperació compte</title>
+                <style>
+                    body {
+                        font-family: Arial, sans-serif;
+                    }
+                    .title {
+                        color: #2c3e50;
+                    }
+                    h1 {
+                        font-size: 24px;
+                    }
+                    p {
+                        font-size: 16px;
+                    }
+                </style>
+            </head>
+            <body>
+                <h1 class="title">Bobby Cotxes: Recuperació compte</h1>
+                <p>Bon dia, ${dades.nombre},</p>
+                <p>Hem rebut una petició per canviar la contrasenya del teu compte d'usuari ${dades.nombre}.</p>
+                <p>Ho podràs recuperar amb el següent enllaç: <a href="http://localhost:4200/recuperacio">recuperacio</a></p>
+                <p>Dada de enviament del correu: <span id="dataEnviament"></span></p>
+
+                <script>
+                    function dataA() {
+                        document.getElementById('dataEnviament').innerHTML = new Date().toUTCString();
+                    }
+                    dataA();
+                </script>
+            </body>
+        </html>`
     };
 
-    await transporter.sendMail(mailOptions, function (error, info) {
+    await transporter.sendMail(mailOptions, function (error, inf) {
         if (error) {
             console.log(error);
         } else {
-            console.log('correu enviat: ' + info.response);
+            console.log('correu enviat: ' + inf.response);
         }
     });
 });
+
+app.get('/usuaris/recuperacio', async (req, res) => {
+    let resposta = req.query.usuari
+
+    const document = await dbC.doc(resposta).get();
+
+    const dades = document.data();
+
+    console.log(dades)
+
+    res.json(dades.clauUnica)
+
+
+})
 
 app.post('/usuaris/push', async (req, res) => {
     let usuario = req.body
