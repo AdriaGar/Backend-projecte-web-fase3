f4b0d981c119a8fccae25c4fe80bc3b8283c84b8
Òscar Mollinedo Echevarría <oscar.mollinedo@institutvidreres.cat>
2025-02-10 11:55:54 +0100

    Modificar enviament de correu per recuperar per mostrar la clau de recuperacio, crear 3 endpoints, el primer '/usuaris/recuperaciocont' per modificar el usuari amb post i actualitzar la clauUnica i la contrasenya, el segon '/usuaris/mailconfusr' per alhora de crear un compte enviar un correu al usuari per verificar el compte posant a la capçelera el usuari per el frontend i per ultim '/usuaris/confirmarusuari' per pasar un usuari i activar el compte posant a la base de dades el camp 'usuariConfirmat' com true

--- server.js
+++ server.js
@@ -74,7 +74,8 @@ app.post('/mail', async (req, res) => {
                 <h1 class="title">Bobby Cotxes: Recuperació compte</h1>
                 <p>Bon dia, ${dades.nombre},</p>
                 <p>Hem rebut una petició per canviar la contrasenya del teu compte d'usuari ${dades.nombre}.</p>
-                <p>Ho podràs recuperar amb el següent enllaç: <a href="http://localhost:4200/recuperacio">recuperacio</a></p>
+                <p>Ho podràs recuperar amb el següent enllaç: <a href="http://localhost:4200/recuperacio">http://localhost:4200/recuperacio</a></p>
+                <p>Clau recuperacio: ${dades.clauUnica}</p>
                 <p>Dada de enviament del correu: <span id="dataEnviament"></span></p>
 
                 <script>
@@ -85,7 +86,7 @@ app.post('/mail', async (req, res) => {
                 </script>
             </body>
         </html>`
-    };
+    }
 
     await transporter.sendMail(mailOptions, function (error, inf) {
         if (error) {
@@ -107,7 +108,15 @@ app.get('/usuaris/recuperacio', async (req, res) => {
 
     res.json(dades.clauUnica)
 
+})
 
+app.post('/usuaris/recuperaciocont', async (req, res) => {
+    temp = req.body
+    console.log(temp)
+    console.log(temp.usuari)
+    console.log(temp.contrasena)
+    console.log(temp.clauUnica)
+    let pujada = await dbC.doc(temp.usuari).update({contrasena: temp.contrasena,clauUnica: temp.clauUnica})
 })
 
 app.post('/usuaris/push', async (req, res) => {
@@ -116,6 +125,76 @@ app.post('/usuaris/push', async (req, res) => {
     let dades = usuario.dades
     let pujada = await dbC.doc(nomDocument).set(dades)
 });
+
+app.post('/usuaris/mailconfusr', async (req, res) => {
+    let usuari = req.body;
+    
+    console.log(usuari.usuariid)
+
+    const document = await dbC.doc(usuari.usuariid).get();
+
+    const dades = document.data();
+    
+    console.log(dades)
+
+    let mailOptions = {
+        from: 'BobbyCotxes@cotxesbobby.com',
+        to: dades.correo,
+        subject: 'Correu confirmacio nou compte de ECommerce Bobby Cotxes',
+        html: `<!DOCTYPE html>
+        <html lang="ca">
+            <head>
+                <meta charset="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Bobby Cotxes: Confirmar nou compte</title>
+                <style>
+                    body {
+                        font-family: Arial, sans-serif;
+                    }
+                    .title {
+                        color: #2c3e50;
+                    }
+                    h1 {
+                        font-size: 24px;
+                    }
+                    p {
+                        font-size: 16px;
+                    }
+                </style>
+            </head>
+            <body>
+                <h1 class="title">Bobby Cotxes: Confirmar nou compte</h1>
+                <p>Bon dia, ${dades.nombre},</p>
+                <p>Benvingut a Bobby Cotxes ${dades.nombre}!.</p>
+                <p>Per poder fer servir el teu compte hauras de confirmar clicant el següent enllaç per fer servir el compte</p>
+                <p><a href="http://localhost:4200/confirmacioCompte?usuari=${encodeURIComponent(dades.usuario)}">confirmar</a></p>
+                <p>Dada de enviament del correu: <span id="dataEnviament"></span></p>
+
+                <script>
+                    function dataA() {
+                        document.getElementById('dataEnviament').innerHTML = new Date().toUTCString();
+                    }
+                    dataA();
+                </script>
+            </body>
+        </html>`
+    }
+
+    await transporter.sendMail(mailOptions, function (error, inf) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('correu enviat: ' + inf.response);
+        }
+    });
+});
+
+app.put('/usuaris/confirmarusuari', async (req, res) => {
+    temp = req.body
+    console.log(temp.usuari)
+    let pujada = await dbC.doc(temp.usuari).update({usuariConfirmat: true})
+})
+
 
 app.get('/usuaris/informaciopersonal',async (req, res) => {
     const documentid = req.query.usuariId;
